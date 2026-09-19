47d2686b58dc7ea3a3736e23a1adfd8f14c1c331
shreeyuvan2020 <shreeyuvan2020@gmail.com>
2026-09-18 19:27:22 -0700

    added angle calculation stuff(just making random numbers and making sure they adhere to the laws of triangle) and then used the law of sines to make those into sides. added print statemnts to make sure im actually making correct triangles.

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import pybooklid
 import streamlit
+import math
 from PIL import Image,ImageDraw
 import random
 sensor = pybooklid.LidSensor()
 for angle in sensor.monitor():
     streamlit.write(f"Lid angle: {angle} degrees")
-    base = random.randint(0, 178)
-    img = Image.new('RGB', (255,255))
-    draw = ImageDraw.Draw(img)
+    base_angle = random.randint(0, 178)
+    second_angle = random.randint(0, 180-base_angle-1)
+    third_angle = 180-second_angle
+    base = 100
+    second = 100 * math.sin(base_angle) / math.sin(second_angle)
+    third = second * math.sin(second_angle) / math.sin(third_angle)
+    print(base_angle, second_angle, third_angle)
+    print(base, second, third)
+
