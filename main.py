b950e67b967337f0f040065d21e576f53423606e
shreeyuvan2020 <shreeyuvan2020@gmail.com>
2026-09-18 19:47:22 -0700

    hella debugging, turns out my math skills aint as good as i thought it would be. first i changed the angle code to subtract both angles from 180 instead of just 1(how tf did i miss that). then i changed up the law of sines code by first converting everything into radians, which math.sin expects and then flipping the math stuff since i did it the wrong way

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ import random
 sensor = pybooklid.LidSensor()
 for angle in sensor.monitor():
     ## streamlit.write(f"Lid angle: {angle} degrees")
-    base_angle = random.randint(0, 178)
-    second_angle = random.randint(0, 180-base_angle-1)
-    third_angle = 180-second_angle
+    base_angle = random.randint(0, 180)
+    second_angle = random.randint(0, 180-base_angle)
+    third_angle = 180-second_angle-base_angle
     base = 100
-    second = 100 * math.sin(base_angle) / math.sin(second_angle)
-    third = second * math.sin(second_angle) / math.sin(third_angle)
+    second = abs(100 * math.sin(math.radians(second_angle)) / math.sin(math.radians(base_angle)))
+    third = abs(second * math.sin(math.radians(third_angle)) / math.sin(math.radians(base_angle)))
     print(base_angle, second_angle, third_angle)
     print(base, second, third)
 
