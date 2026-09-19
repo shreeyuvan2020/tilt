b8a8b61025af054148f7648650441f8cb2580872
shreeyuvan2020 <shreeyuvan2020@gmail.com>
2026-09-18 19:28:16 -0700

    nvm forgor to comment out the code that makes it a streamlit app, i just want to debug for now

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from PIL import Image,ImageDraw
 import random
 sensor = pybooklid.LidSensor()
 for angle in sensor.monitor():
-    streamlit.write(f"Lid angle: {angle} degrees")
+    ## streamlit.write(f"Lid angle: {angle} degrees")
     base_angle = random.randint(0, 178)
     second_angle = random.randint(0, 180-base_angle-1)
     third_angle = 180-second_angle
